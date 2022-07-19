7f2e30f3640f03c394539fc94e4b69d6f23fbcca
Sachin <heysachinkry@gmail.com>
2022-07-19 09:20:13 +0530

    final commit

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x7A7Ab7DBE1EF0A8bdf0dBE58BaECfc88b359f1A7";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-dapp-sand.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
